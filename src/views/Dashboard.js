1a44dc3e2b0666d4932feff768a86a520464f51b
Sebastian Villarroel <sebastian.villarroel@reign.cl>
2020-02-17 15:00:19 -0300

    Modificacion Login

---
+++ src/views/Dashboard.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import './Welcome.css';
+
+function Welcome() {
+    return(
+        <div className="Container Welcome-container">
+            This is the dashboard
+        </div>
+    )
+
+}
+export default Welcome;
